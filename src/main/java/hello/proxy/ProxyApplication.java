5e994e28c6b0ded976645fad3cf19fa741cbf51a
highjune <highjune37@gmail.com>
2023-08-17 00:06:36 +0900

    mvc 구현 클래스들을 수동으로 @Bean 등록하기

--- src/main/java/hello/proxy/ProxyApplication.java
+++ src/main/java/hello/proxy/ProxyApplication.java
@@ -1,8 +1,14 @@
 package hello.proxy;
 
+import hello.proxy.config.AppV1Config;
+import hello.proxy.config.AppV2Config;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
 
+//@Import(AppV1Config.class) // AppV1Config 가 @Bean 으로 등록이 되어야 AppV1Config 내의 @Bean들도 등록이 되므로.
+@Import(AppV2Config.class) // AppV1Config 가 @Bean 으로 등록이 되어야 AppV1Config 내의 @Bean들도 등록이 되므로.
 @SpringBootApplication(scanBasePackages = "hello.proxy.app") //주의
 public class ProxyApplication {
 
