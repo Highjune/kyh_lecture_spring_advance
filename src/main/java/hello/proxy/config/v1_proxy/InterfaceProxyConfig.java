43ca75b6f905002f211e520e4792d7ba90b71def
highjune <highjune@V-Dev-KangJuneui-MacBookPro.local>
2023-08-18 19:32:29 +0900

    인터페이스 기반 프록시를 적용해서 로그 찍는 것 완료

---
+++ src/main/java/hello/proxy/config/v1_proxy/InterfaceProxyConfig.java
@@ -0,0 +1,35 @@
+package hello.proxy.config.v1_proxy;
+
+import hello.proxy.app.v1.*;
+import hello.proxy.config.v1_proxy.interface_proxy.OrderControllerInterfaceProxy;
+import hello.proxy.config.v1_proxy.interface_proxy.OrderRepositoryInterfaceProxy;
+import hello.proxy.config.v1_proxy.interface_proxy.OrderServiceInterfaceProxy;
+import hello.proxy.trace.logtrace.LogTrace;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class InterfaceProxyConfig {
+
+    // 실제 객체는 스프링 Bean으로 등록하면 안됨.
+    // 스프링 빈으로 실제 객체 대신에 프록시 객체를 등록했기 때문에 앞으로 스프링 빈을 주입 받으면 실제 객체 대신에 프록시 객체가 주입된다.
+    // 실제 객체가 스프링 빈으로 등록되지 않는다고 해서 사라지는 것은 아니다. 프록시 객체가 실제 객체를 참조하기 때문에 프록시를 통해서 실제 객체를 호출할 수 있다. 쉽게 이야기해서 프록시 객체 안에 실제 객체가 있는 것이다.
+
+    @Bean
+    public OrderControllerV1 orderController(LogTrace logTrace) {
+        OrderControllerV1Impl controllerImpl = new OrderControllerV1Impl(orderService(logTrace));
+        return new OrderControllerInterfaceProxy(controllerImpl, logTrace); // Proxy를 Bean으로 등록함. proxy가 실제 객체 controllerImpl를 주입받아서 호출함.
+    }
+
+    @Bean
+    public OrderServiceV1 orderService(LogTrace logTrace) {
+        OrderServiceV1Impl serviceImpl = new OrderServiceV1Impl(orderRepository(logTrace));
+        return new OrderServiceInterfaceProxy(serviceImpl, logTrace); // Proxy를 Bean으로 등록함. proxy가 실제 객체 serviceImpl 주입받아서 호출함
+    }
+
+    @Bean
+    public OrderRepositoryV1 orderRepository(LogTrace logTrace) {
+        OrderRepositoryV1Impl repositoryImpl = new OrderRepositoryV1Impl();
+        return new OrderRepositoryInterfaceProxy(repositoryImpl, logTrace); // Proxy를 Bean으로 등록함. proxy가 실제 객체 repositoryImpl 주입받아서 호출함
+    }
+}
